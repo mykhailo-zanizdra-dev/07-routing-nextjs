d05200bd4015bf03efafe613eb1419abb9048ece
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-10-28 00:19:39 +0200

    fix: Fixed sidebar

--- components/SidebarNotes/SidebarNotes.tsx
+++ components/SidebarNotes/SidebarNotes.tsx
@@ -1,17 +1,32 @@
+'use client';
+import clsx from 'clsx';
 import Link from 'next/link';
 import css from './SidebarNotes.module.css';
 import { noteTags } from '@/types/note';
+import { useParams } from 'next/navigation';
 
-async function SidebarNotes() {
+function SidebarNotes() {
+  const { slug: [tag] = [] } = useParams();
+  const currentTag = (tag || 'all').toLowerCase();
+
   return (
     <ul className={css.menuList}>
-      <li className={css.menuItem}>
+      <li
+        className={clsx(css.menuItem, {
+          [css.menuLinkActive]: currentTag === 'all',
+        })}
+      >
         <Link href={`/notes/filter/all`} className={css.menuLink}>
           All notes
         </Link>
       </li>
       {noteTags.map(tag => (
-        <li key={tag} className={css.menuItem}>
+        <li
+          key={tag}
+          className={clsx(css.menuItem, {
+            [css.menuLinkActive]: currentTag === tag.toLowerCase(),
+          })}
+        >
           <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
             {tag}
           </Link>
